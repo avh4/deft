6f44bbc4affec66a8b4c8f94a7214de358f0c28b
Aaron VonderHaar <gruen0aermel@gmail.com>
2013-06-08 14:01:37 -0700

    Update project metadata.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject deft "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject net.avh4.clj/deft "0.1.0-SNAPSHOT"
+  :description "An experimental API design for functional GUI development"
+  :url "http://github.com/avh4/deft"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])

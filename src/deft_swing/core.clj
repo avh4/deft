2af94f512cad325a20f0ea0c52b4c8923906b7fa
Aaron VonderHaar <gruen0aermel@gmail.com>
2013-06-09 23:23:27 -0700

    Use extend-type instead of extend in deft-swing.core

--- src/deft_swing/core.clj
+++ src/deft_swing/core.clj
@@ -18,35 +18,35 @@
   ([font-name pt] (.deriveFont (get-font font-name) (float pt)))
   ([font-name] (Font/createFont Font/TRUETYPE_FONT (java.io.FileInputStream. (str font-name ".ttf")))))))
 
-(extend java.awt.Graphics2D
+(extend-type java.awt.Graphics2D
   FontService
-  { :load-font (fn [gc font-name pt]
-      (get-font font-name pt))
-    :text-width (fn [gc font string]
-      (.getAdvance (TextLayout. string font (.getFontRenderContext gc))))
-    }
+  (load-font [gc font-name pt]
+    (get-font font-name pt))
+  (text-width [gc font string]
+    (.getAdvance (TextLayout. string font (.getFontRenderContext gc))))
+
   ColorService
-  { :load-color (fn [gc deft-color]
-      (let [r (red-value   deft-color)
-            g (green-value deft-color)
-            b (blue-value  deft-color)]
-        (get-color r g b)))
-    }
+  (load-color [gc deft-color]
+    (let [r (red-value   deft-color)
+          g (green-value deft-color)
+          b (blue-value  deft-color)]
+      (get-color r g b)))
+
   GraphicsContext
-  { :prepare (fn [gc]
+  (prepare [gc]
     (.setRenderingHints gc
       {RenderingHints/KEY_ANTIALIASING RenderingHints/VALUE_ANTIALIAS_ON
        RenderingHints/KEY_TEXT_ANTIALIASING RenderingHints/VALUE_TEXT_ANTIALIAS_LCD_HRGB
        RenderingHints/KEY_FRACTIONALMETRICS RenderingHints/VALUE_FRACTIONALMETRICS_ON
        }))
-    :fill-rect (fn [gc [x y w h] color]
-      (.setColor gc color)
-      (.fillRect gc x y w h))
-    :fill-oval (fn [gc [x y w h] color]
-      (.setColor gc color)
-      (.fillOval gc x y w h))
-    :draw-string (fn [gc string left-x baseline-y font color]
-      (.setFont gc font)
-      (.setColor gc color)
-      (.drawString gc string (float left-x) (float baseline-y)))
-    })
+  (fill-rect [gc [x y w h] color]
+    (.setColor gc color)
+    (.fillRect gc x y w h))
+  (fill-oval [gc [x y w h] color]
+    (.setColor gc color)
+    (.fillOval gc x y w h))
+  (draw-string [gc string left-x baseline-y font color]
+    (.setFont gc font)
+    (.setColor gc color)
+    (.drawString gc string (float left-x) (float baseline-y)))
+  )

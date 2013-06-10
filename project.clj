6ca5d2862778e4dc4d4f89002bb715d773562859
Aaron VonderHaar <gruen0aermel@gmail.com>
2013-06-09 22:46:27 -0700

    Move SWT backend to deft-swt.
    http://github.com/avh4/deft-swt

--- project.clj
+++ project.clj
@@ -3,9 +3,7 @@
   :url "http://github.com/avh4/deft"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :repositories {"swt-repo" "https://swt-repo.googlecode.com/svn/repo/"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.eclipse.swt/org.eclipse.swt.cocoa.macosx.x86_64 "4.2.2"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]]
   :profiles {
     :dev {
       :plugins [[lein-midje "3.0.0"]]

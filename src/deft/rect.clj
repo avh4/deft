185d02be2adc8998a7c69a384c12374a91104589
Aaron VonderHaar <gruen0aermel@gmail.com>
2013-06-10 22:45:17 -0700

    Add deft.rect/of-size

--- src/deft/rect.clj
+++ src/deft/rect.clj
@@ -3,3 +3,5 @@
 (defn from-center 
   ([x y size] (from-center x y size size))
   ([x y w h] [(- x (/ w 2)) (- y (/ h 2)) w h]))
+
+(defn of-size [w h] [0 0 w h])
--- test/deft/rect_test.clj
+++ test/deft/rect_test.clj
@@ -3,8 +3,10 @@
   (:use [midje sweet]))
 
 (facts "about rect operations"
-  (fact "about from-center"
+  (fact "from-center makes rects of a given size centered at a point"
     (from-center 0 0 2) => [-1 -1 2 2]
     (from-center 10 10 2) => [9 9 2 2]
     (from-center 0 0 2 4) => [-1 -2 2 4]
-    (from-center 10 10 2 4) => [9 8 2 4]))
+    (from-center 10 10 2 4) => [9 8 2 4])
+  (fact "of-size makes rects at the origin of the given size"
+    (of-size 10 20) => [0 0 10 20]))

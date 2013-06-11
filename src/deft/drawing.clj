c89308566dcc7b20a0b513d25b1096022350f5ba
Aaron VonderHaar <gruen0aermel@gmail.com>
2013-06-10 19:24:19 -0700

    Allow drawing of seqs.

--- src/deft/drawing.clj
+++ src/deft/drawing.clj
@@ -17,13 +17,20 @@
     :left-x left-x :baseline-y baseline-y
     :color color}) )
 
-(defmulti draw (fn [s _] (if (nil? s) :null (:shape s))))
+(defmulti draw (fn [s _]
+  (cond
+    (nil? s) :-null
+    (:shape s) (:shape s)
+    (sequential? s) :-seq
+    :else :-null)))
 (defmethod draw :rect [{color :color [x y w h] :bounds} gc]
   (fill-rect gc [x y w h] (load-color gc color)))
 (defmethod draw :oval [{color :color [x y w h] :bounds} gc]
   (fill-oval gc [x y w h] (load-color gc color)))
 (defmethod draw :text [{:keys [text font font-size color left-x baseline-y]} gc]
   (draw-string gc text left-x baseline-y (load-font gc font font-size) (load-color gc color)))
-(defmethod draw :null [s _]
+(defmethod draw :-seq [s-seq gc]
+  (doseq [s s-seq] (draw s gc)))
+(defmethod draw :-null [s _]
   (print "WARNING: No draw method for ")
   (prn s))
